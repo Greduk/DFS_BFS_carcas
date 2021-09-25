199f682100f6119c013e742786e2c1a6942e89fe
nickspb-ui <nickoustinov@gmail.com>
2021-09-25 19:15:55 +0300

    123

--- main.py
+++ main.py
@@ -9,18 +9,34 @@ f = open('input.txt', 'r')
 n = [[int(j) for j in list(filter(None, i.split(' ')))] for i in list(filter(None, f.read().split('\n')))]
 f.close()
 
-f = open('output_DFS.txt', 'w')
+f = open('output_BFS.txt', 'w')
 while len(stack) > 0:
 	for i in stack:
 		f.write(str(i) + ' ')
 	f.write('\n')
-	tmp = stack.pop()
+	tmp = stack.pop(0)
 	for i in range(len(n[tmp])):
 		if n[tmp][i] == 1 and i not in checked_nodes:
 			stack.append(i)
 			checked_nodes.append(i)
 
 f.close()
+
+checked_nodes = [0]
+stack = [0]
+f = open('output_DFS.txt', 'w')
+while len(stack) > 0:
+	for i in stack:
+		f.write(str(i) + ' ')
+	f.write('\n')
+	tmp = stack.pop()
+	for i in range(len(n[tmp]) - 1, -1, -1):
+		if n[tmp][i] == 1 and i not in checked_nodes:
+			stack.append(i)
+			checked_nodes.append(i)
+
+f.close()
+
 q = sets.adjacency_to_set(n)
 
 q = [list(i) for i in q]
@@ -32,5 +48,5 @@ for i in range(len(q)):
 		if q[i][j] > i:
 			dot.edge(str(i), str(q[i][j]), arrowhead='none')
 
-dot.render('graph.gv', view=True)
+dot.render('graph.gv', view=False)
 print('Graph converted successfully.')
