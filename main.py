28041e1e92c489919f34b473243250ff7b0e9df9
nickspb-ui <nickoustinov@gmail.com>
2021-09-28 19:21:57 +0300

    123

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sets
 import os
 os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin\\'
 
-checked_nodes = [0]
+checked_nodes = {0: 0}
 stack = [0]
 f = open('input.txt', 'r')
 n = [[int(j) for j in list(filter(None, i.split(' ')))] for i in list(filter(None, f.read().split('\n')))]
@@ -18,11 +18,28 @@ while len(stack) > 0:
 	for i in range(len(n[tmp])):
 		if n[tmp][i] == 1 and i not in checked_nodes:
 			stack.append(i)
-			checked_nodes.append(i)
+			checked_nodes[i] = tmp
 
+dot1 = graphviz.Digraph(comment='Carcas')
+for i in range(len(n)):
+	dot1.node(str(i))
+for i in range(len(n)):
+	if i == 0:
+		continue
+	dot1.edge(str(i), str(checked_nodes[i]), arrowhead='none')
+
+dot1.render('karkas_bfs.gv', view=True)
+
+del checked_nodes[0]
+
+for i in range(1, len(n)):
+	f.write(str(i) + ' ')
+f.write('\n')
+for i in range(1, len(n)):
+	f.write(str(checked_nodes[i]) + ' ')
 f.close()
 
-checked_nodes = [0]
+checked_nodes = {0: 0}
 stack = [0]
 f = open('output_DFS.txt', 'w')
 while len(stack) > 0:
@@ -33,7 +50,13 @@ while len(stack) > 0:
 	for i in range(len(n[tmp]) - 1, -1, -1):
 		if n[tmp][i] == 1 and i not in checked_nodes:
 			stack.append(i)
-			checked_nodes.append(i)
+			checked_nodes[i] = tmp
+
+for i in range(1, len(n)):
+	f.write(str(i) + ' ')
+f.write('\n')
+for i in range(1, len(n)):
+	f.write(str(checked_nodes[i]) + ' ')
 
 f.close()
 
@@ -49,4 +72,15 @@ for i in range(len(q)):
 			dot.edge(str(i), str(q[i][j]), arrowhead='none')
 
 dot.render('graph.gv', view=False)
+
+dot1 = graphviz.Digraph(comment='Carcas')
+for i in range(len(n)):
+	dot1.node(str(i))
+for i in range(len(n)):
+	if i == 0:
+		continue
+	dot1.edge(str(i), str(checked_nodes[i]), arrowhead='none')
+
+dot1.render('karkas.gv', view=True)
+
 print('Graph converted successfully.')
