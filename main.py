0be0fdeb1fe9ac2c7a2a53465297f4f3e6e2b124
nickspb-ui <nickoustinov@gmail.com>
2021-10-16 17:57:50 +0300

    123

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 import copy
 os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin\\'
 
-checked_nodes = {0: 0}
-stack = [0]
+checked_nodes = {}
+stack = []
 f = open('input.txt', 'r')
 n = [[int(j) for j in list(filter(None, i.split(' ')))] for i in list(filter(None, f.read().split('\n')))]
 f.close()
@@ -19,18 +19,28 @@ for i in range(len(q)):
 for i in range(len(q)):
 	for j in range(len(q[i])):
 		if q[i][j] > i:
-			dot.edge(str(i), str(q[i][j]), arrowhead='none')
+			dot.edge(str(i), str(q[i][j]), arrowhead='none', color='white')
 
 f = open('output_BFS.txt', 'w')
-while len(stack) > 0:
-	for i in stack:
-		f.write(str(i) + ' ')
-	f.write('\n')
-	tmp = stack.pop(0)
-	for i in range(len(n[tmp])):
-		if n[tmp][i] == 1 and i not in checked_nodes:
-			stack.append(i)
-			checked_nodes[i] = tmp
+for qq in range(len(n)):
+	if qq not in checked_nodes:
+		stack.append(qq)
+		checked_nodes[qq] = qq
+		for zxc in stack:
+			f.write(str(zxc) + ' ')
+		f.write('\n')
+		while len(stack) > 0:
+			tmp = stack.pop(0)
+			for zxc in stack:
+				f.write(str(zxc) + ' ')
+			f.write('\n')
+			for i in range(len(n[tmp])):
+				if n[tmp][i] == 1 and i not in checked_nodes:
+					stack.append(i)
+					checked_nodes[i] = tmp
+					for zxc in stack:
+						f.write(str(zxc) + ' ')
+					f.write('\n')
 
 dot1 = copy.deepcopy(dot)
 for i in range(len(n)):
@@ -38,7 +48,7 @@ for i in range(len(n)):
 		continue
 	dot1.edge(str(i), str(checked_nodes[i]), arrowhead='none', color='red')
 
-dot1.render('karkas_bfs.gv', view=True)
+dot1.render('karkas_bfs.gv', view=False)
 
 del checked_nodes[0]
 
@@ -49,18 +59,28 @@ for i in range(1, len(n)):
 	f.write(str(checked_nodes[i]) + ' ')
 f.close()
 
-checked_nodes = {0: 0}
-stack = [0]
+checked_nodes = {}
+stack = []
 f = open('output_DFS.txt', 'w')
-while len(stack) > 0:
-	for i in stack:
-		f.write(str(i) + ' ')
-	f.write('\n')
-	tmp = stack.pop()
-	for i in range(len(n[tmp]) - 1, -1, -1):
-		if n[tmp][i] == 1 and i not in checked_nodes:
-			stack.append(i)
-			checked_nodes[i] = tmp
+for qq in range(len(n)):
+	if qq not in checked_nodes:
+		stack.append(qq)
+		checked_nodes[qq] = qq
+		for zxc in stack:
+			f.write(str(zxc) + ' ')
+		f.write('\n')
+		while len(stack) > 0:
+			tmp = stack.pop()
+			for zxc in stack:
+				f.write(str(zxc) + ' ')
+			f.write('\n')
+			for i in range(len(n[tmp]) - 1, -1, -1):
+				if n[tmp][i] == 1 and i not in checked_nodes:
+					stack.append(i)
+					checked_nodes[i] = tmp
+					for zxc in stack:
+						f.write(str(zxc) + ' ')
+					f.write('\n')
 
 for i in range(1, len(n)):
 	f.write(str(i) + ' ')
@@ -76,6 +96,6 @@ for i in range(len(n)):
 		continue
 	dot2.edge(str(i), str(checked_nodes[i]), arrowhead='none', color='blue')
 
-dot2.render('karkas_dfs.gv', view=True)
+dot2.render('karkas_dfs.gv', view=False)
 
 print('Graph converted successfully.')
